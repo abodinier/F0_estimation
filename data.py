0f7e823708f4ef0226b8a4d0924f25b72e210e3d
Alexandre Bodinier <alexandrebodinier@icloud.com>
2022-08-25 23:29:11 +0200

    Create data class

---
+++ data.py
@@ -0,0 +1,24 @@
+import torch
+import numpy as np
+from pathlib import Path
+from torch.utils.data import Dataset
+
+class SalienceDataset(Dataset):
+    def __init__(self, data_dir, ratio=1):
+        if not isinstance(data_dir, Path):
+            data_dir = Path(data_dir)
+        assert data_dir.is_dir(), f"{data_dir} not found"
+        
+        self.ratio = ratio
+        self.data_dir = data_dir
+        self.data_list = list(data_dir.glob("*.npz"))
+    
+    def __len__(self):
+        return int(len(self.data_list) * self.ratio)
+    
+    def __getitem__(self, index):
+        data = np.load(self.data_list[index])
+        return [
+            torch.tensor(data["hcqt"], dtype=torch.float32),
+            torch.tensor(data["target_salience"], dtype=torch.float32)
+        ]
