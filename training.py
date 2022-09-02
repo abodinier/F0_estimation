741aba9ad2a08368081179f2c3cacf3d95663c33
Alexandre Bodinier <alexandrebodinier@icloud.com>
2022-09-02 10:19:20 +0200

    Just to be sure pred and target in good order

--- training.py
+++ training.py
@@ -60,7 +60,7 @@ class Trainer:
                 y.to(self.device)
                 
                 out = self.model(x)
-                loss = self.loss_cls(out, y)
+                loss = self.loss_cls(input=out, target=y)
                 batch_losses.append(loss.item())
         
         metrics = evaluate(self.model, self.val_data)
