19a4d9a452c9432244bcbaa63c3434d8b65fcdf6
Alexandre Bodinier <alexandrebodinier@icloud.com>
2022-09-06 00:02:34 +0200

    resume training

--- training.py
+++ training.py
@@ -1,11 +1,17 @@
 import torch
 import numpy as np
 from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
 
 from utils import visualize, evaluate
+from models import load_from_dir
+from data import SalienceDataset
+from torch.utils.data import DataLoader
+from torch.nn import BCEWithLogitsLoss
+from torch.optim import Adam
 
 class Trainer:
-    def __init__(self, model, train_data, val_data, loss_cls, optimizer, summary_writer, ckp_path, device="cpu"):
+    def __init__(self, model, train_data, val_data, loss_cls, optimizer, ckp_path=None, summary_writer=None, device="cpu"):
         self.model = model
         self.train_data = train_data
         self.val_data = val_data
@@ -14,6 +20,12 @@ class Trainer:
         self.device = device
         self.summary_writer = summary_writer
         self.ckp_path = ckp_path
+        
+        self.init_trainer()
+        
+    def init_trainer(self):
+        self.best_loss = np.inf
+        self.epoch = 1
         
     def train_step(self):
         self.model.to(self.device)
@@ -91,24 +103,72 @@ class Trainer:
     def train(self, n_epochs):
         self.model.to(self.device)
         
-        best_loss = np.inf
-        
-        epoch = 1
-        while epoch < n_epochs:
-            print(f"EPOCH [{epoch}/{n_epochs}]")
-            self.epoch = epoch
+        while self.epoch < n_epochs:
+            print(f"EPOCH [{self.epoch}/{n_epochs}]")
             
             train_loss = self.train_step()
-            self.summary_writer.add_scalar("train/loss", train_loss, epoch)
+            self.summary_writer.add_scalar("train/loss", train_loss, self.epoch)
             
             val_loss = self.val_step()
-            self.summary_writer.add_scalar("val/loss", val_loss, epoch)
+            self.summary_writer.add_scalar("val/loss", val_loss, self.epoch)
             
-            if val_loss < best_loss:
-                torch.save(self.model.state_dict(), self.ckp_path/"ckp.pt")
+            self.checkpoint(val_loss)
             
             print(f"\tTrain loss : {train_loss} | Val loss : {val_loss}")
             
-            epoch += 1
+            self.epoch += 1
         
         print("Training complete.")
+    
+    def checkpoint(self, val_loss):
+        if val_loss < self.best_loss:
+            self.best_loss = val_loss
+            ckp = {
+                "epoch": self.epoch,
+                "optimizer": self.optimizer.state_dict(),
+                "model": self.model.state_dict(),
+                "best_loss": self.best_loss
+            }
+            torch.save(ckp, self.ckp_path)
+        else:
+            pass
+    
+    def load_from_checkpoint(self, ckp):      
+        self.optimizer.load_state_dict(ckp["optimizer"])
+        self.model.load_state_dict(ckp["model"])
+        self.epoch = ckp["epoch"]
+        self.best_loss = ckp["best_loss"]
+        
+        return self
+    
+    @staticmethod
+    def load_from_cfg(cfg):
+        model_path = cfg["MODEL_PATH"]
+        model = load_from_dir(model_path)
+        
+        train_data = SalienceDataset(cfg["TRAIN_DATA_DIR"], ratio=cfg["DATA_RATIO"])
+        val_data = SalienceDataset(cfg["VALIDATION_DATA_DIR"], ratio=cfg["DATA_RATIO"])
+        train_loader = DataLoader(train_data, batch_size=cfg["BATCH_SIZE"], shuffle=True)
+        val_loader = DataLoader(val_data, batch_size=cfg["BATCH_SIZE"], shuffle=True)
+        
+        loss_cls = BCEWithLogitsLoss()
+        optimizer = Adam(lr=cfg["LR"], params=model.parameters(), weight_decay=cfg["WEIGHT_DECAY"])
+        
+        device = cfg["DEVICE"]
+        
+        summary_writer = SummaryWriter(cfg["TENSORBOARD_DIR"])
+        ckp_path = cfg["CKP_PATH"]
+        ckp = torch.load(ckp_path)
+        
+        instance = Trainer(
+            model=model,
+            train_data=train_loader,
+            val_data=val_loader,
+            loss_cls=loss_cls,
+            optimizer=optimizer,
+            device=device,
+            summary_writer=summary_writer,
+            ckp_path=ckp_path
+        ).load_from_checkpoint(ckp)
+        
+        return instance
