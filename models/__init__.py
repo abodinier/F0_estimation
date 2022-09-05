d1d5e187c2014bfc1330081c635de148b4fddadf
Alexandre Bodinier <alexandrebodinier@icloud.com>
2022-09-06 00:02:22 +0200

    Load model from pickle

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,18 @@
+import pickle
+
 from .salience_network import SalienceNetwork
 from .harmonicCNN import HarmonicCNN
 from .unet import UNet
+
 
 __all__ = [
     "SalienceNetwork",
     "HarmonicCNN",
     "UNet"
 ]
+
+
+def load_from_dir(path):
+    with open(path, "rb") as f:
+        model = pickle.load(f)
+    return model
